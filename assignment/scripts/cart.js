3fb5a32e3ab14e825cddf84bc25e1f2f44d2af94
Swaggjet <asantesimmons@gmail.com>
2021-10-27 18:15:57 -0500

    Prime Assignments/prime-pw-week-4-assignment-part-3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,18 +1,30 @@
 console.log('***** Cart Functions *****');
 
 
-function isFull(){  //creating a function to loop through array to check if it empty
+let basket = []
+const maxItems = 5
+
+ //creating a function to loop through array to check if it empty
+function isFull(){
   for ( let i = 0; i < basket.length; i++){
-  if (basket.lenght <= maxItems) {
+  if (basket[i] <= maxItems) {
       return true;
     } else {
       return false;
   }
+}
+
+
+  function addItem(item){
+    basket.push(item);
+    return true;
+  }
+
 
 console.log('basket is:', basket );
-console.log('add some apples to the basket',
- addItem('sandwich');
- addItem('apples');
+console.log('add some apples to the basket')
+console.log(addItem(`sandwich`));
+console.log(addItem(`apples`));
 console.log(`Checking added {basket}`, basket);
 
   //creating function to list items in basket
@@ -22,29 +34,22 @@ function listItems() {
   }
 }
 
+listitem()
+
+console.log('basket is:', basket);
+console.log(addItem('baseball'));
+console.log(addItem('tennis ball'));
+console.log(addItem('basketball'));
+console.log(addItem('football'));
+console.log(addItem('javalin'));
+console.log(addItem('golfball'));
+
+function empty(){
+  let items = basket.splice(0, basket.length);
+  return items;
+
+}
+console.log('basket is', basket);
 console.log('check to see if items are deleted ', empty());
 console.log('basket is now empty',basket);
 console.log('no room for the items', isFull());
-
-// Make sure to test all functions here in the JS file!
-// We want to see how you are testing your code!!!
-
-let basket = []
-const maxItems = 5;
-
-}
-
-function addItem(item){
-  basket.push(item);
-  return true;
-}
-
-//----DRIVER CODE------------------------------------------------
-console.log('basket is:', basket);
-addItem('baseball')
-addItem('tennis ball');
-addItem('basketball');
-addItem('football');
-addItem('javalin');
-
-console.log('basket is', basket);
